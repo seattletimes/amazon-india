710d558475c1a6862f41fca2d6b44814039397ac
Audrey Carlsen <acarlsen@seattletimes.com>
2015-09-28 15:26:57 -0700

    hello world

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+// require("./lib/social");
+// require("./lib/ads");
+// var track = require("./lib/tracking");
+
+require("component-responsive-frame/child");
+
+var dot = require("./lib/dot");
+var slide = dot.compile(require("./_slide.html"));
+
+var index = 0;
+
+var changeImage = function() {
+  if (index == data.length) { 
+    document.querySelector(".next").classList.add("disabled");
+  } else {
+    document.querySelector(".next").classList.remove("disabled");
+  }
+  if (index == 0) { 
+    document.querySelector(".previous").classList.add("disabled");
+  } else {
+    document.querySelector(".previous").classList.remove("disabled");
+  }
+  document.querySelector(".caption").innerHTML = data[index].caption;
+  document.querySelector(".image").innerHTML = `<img src="http://placekitten.com/g/960/630">`;
+};
+
+changeImage();
+
+document.querySelector(".slide-container").addEventListener("click", function(e) {
+  if (e.target.classList.contains("next") && index < data.length) index += 1;
+  if (e.target.classList.contains("previous") && index > 0) index -= 1;
+  changeImage();
+});
