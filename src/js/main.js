873028966b10ff98e1f407af98b696b5beb8eabb
Audrey Carlsen <acarlsen@seattletimes.com>
2015-10-01 15:54:41 -0700

    rewind (whitney)

--- src/js/main.js
+++ src/js/main.js
@@ -13,14 +13,16 @@ var length = data.length - 1;
 
 var changeImage = function() {
   if (index == length) { 
-    document.querySelector(".next").classList.add("disabled");
+    document.querySelector(".next").classList.add("gone");
+    document.querySelector(".rewind").classList.remove("gone");
   } else {
-    document.querySelector(".next").classList.remove("disabled");
+    document.querySelector(".next").classList.remove("gone");
+    document.querySelector(".rewind").classList.add("gone");
   }
   if (index == 0) { 
-    document.querySelector(".previous").classList.add("disabled");
+    document.querySelector(".previous").classList.add("hidden");
   } else {
-    document.querySelector(".previous").classList.remove("disabled");
+    document.querySelector(".previous").classList.remove("hidden");
   }
   document.querySelector(".index").innerHTML = `${index + 1} of ${data.length}`;
   document.querySelector(".caption").innerHTML = data[index].caption;
@@ -38,5 +40,6 @@ changeImage();
 document.querySelector(".slide-container").addEventListener("click", function(e) {
   if (e.target.classList.contains("next") && index < length) index += 1;
   if (e.target.classList.contains("previous") && index > 0) index -= 1;
+  if (e.target.classList.contains("rewind")) index = 0;
   changeImage();
 });
